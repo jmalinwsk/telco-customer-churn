bfb21a06c40b6ceedb02b55817f11c71377f0cdd
crin <37940924+crinuyi@users.noreply.github.com>
2020-01-09 23:26:11 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+
+class Main:
+    def main(self):
+        data = pd.read_csv('database\WA_Fn-UseC_-Telco-Customer-Churn.csv')
+
+
+if __name__ == '__main__':
+    Main().main()
